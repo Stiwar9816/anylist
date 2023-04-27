11fbc5f564db0f25ece2124a28afe868902d4897
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2023-04-27 14:21:48 -0500

    Configuración inicial con docker, postgres y GraphQL

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Anylist API
+
+1. Instalar **[NestJS CLI](https://docs.nestjs.com/#installation)** globalmente
+
+2. Clonar el repositorio
+
+3. Instalar las depencias con el comando:
+
+```
+yarn install o npm install
+```
+
+3. Clonar el archivo **`.env.template`** y renombrarlo a **`.env`**
+
+4. Cambiar las variables de entorno
+
+5. Instalar **[Docker Desktop](https://www.docker.com/products/docker-desktop/)** según la versión de tu sistema operativo
+
+6. Levantar la base de datos postgres
+
+```
+docker-compose up -d
+docker-compose up (Para revisar los logs)
+```
+
+7. Levantar el proyecto en modo de desarrollo con el comando:
+
+```
+yarn start:dev
+```
+
+8. Playground de GraphQL con Apollo Studio
+
+```
+localhost:3000/graphql
+```
+
+## Stack Usado
+
+- NestJS
+- Docker
+- Postgres
+- GraphQL
---
+++ src/app.module.ts
@@ -0,0 +1,48 @@
+import { join } from 'path';
+// NestJS
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+// TypeORM
+import { TypeOrmModule } from '@nestjs/typeorm';
+// GraphQL
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+// Modules
+import { ItemsModule } from './items/items.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    // Configuración de credenciales de la DB
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null
+      },
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    // GraphQL
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+      ]
+    }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
